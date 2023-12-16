407d17fe129c2b3da8496197d8c0ce089a0e44bd
arifu11 <arifuddinachmad11@gmail.com>
2023-12-16 17:12:27 +0700

    Persiapan API Get Review

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/enrolledCourseReview', 'EnrolledCourseReview::index');
 $routes->get('/courseView', 'CourseView::index');
+$routes->post('/enrolledCourseReview/createReview', 'EnrolledCourseReview::createReview');
+$routes->get('/courseReviewAPI', 'CourseReviewAPI::getAll');
+
